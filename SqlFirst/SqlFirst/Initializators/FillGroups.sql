05bff64ffa325f9575f61cce1ac827539b2b2e6f
Bohdan <bohdan.hukivskyi@outlook.com>
2020-08-08 11:57:47 +0300

    initial

---
+++ SqlFirst/SqlFirst/Initializators/FillGroups.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO [dbo].[Group]
+(Id, Name)
+VALUES
+(NEWID(), 'YD-01'),
+(NEWID(), 'YD-02'),
+(NEWID(), 'ET-01'),
+(NEWID(), 'ET-02')
---
+++ SqlFirst/SqlFirst/Query/GetStudents.sql
@@ -0,0 +1,3 @@
+﻿SELECT s.Id, s.FirstName, s.LastName, g.Name
+FROM [dbo].Student s
+JOIN [dbo].[Group] g ON g.Id = s.GroupId
